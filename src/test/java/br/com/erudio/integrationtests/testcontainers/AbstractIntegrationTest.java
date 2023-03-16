5d495080c4dbf817278792ffd67661bb1d07dc8c
Natã Lopes Martins <54146961+NathanMarts@users.noreply.github.com>
2023-03-16 07:38:05 -0300

    test

---
+++ src/test/java/br/com/erudio/integrationtests/testcontainers/AbstractIntegrationTest.java
@@ -0,0 +1,43 @@
+package br.com.erudio.integrationtests.testcontainers;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+import org.hibernate.service.spi.Startable;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.lifecycle.Startables;
+
+@ContextConfiguration(inheritInitializers = AbstractIntegrationTest.Initializer.class)
+public class AbstractIntegrationTest {
+    public class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0.29");
+
+        private static void startContainers() {
+            Startables.deepStart(Stream.of(mysql)).join();
+        }
+
+        private Map createConnectionConfiguration() {
+            return Map.of(
+                "spring.datasource.url", mysql.getJdbcUrl(),
+                "spring.datasource.url", mysql.getJdbcUrl(),
+                "spring.datasource.url", mysql.getJdbcUrl(),
+            );
+        }
+
+        @SuppressWarnings({"unchecked", "rawtypes"})
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            startContainers();
+            ConfigurableEnvironment environment = applicationContext.getEnvironment();
+            MapPropertySource testcontainers = new MapPropertySource(
+                "testcontainers", 
+                (Map) createConnectionConfiguration());
+            environment.getPropertySources().addFirst(testcontainers);
+        }
+    }
+}
